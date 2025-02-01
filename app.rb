51c65fccee6e60bfbdfcc0106e4c33de4a1d214e
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-01 21:43:21 +0000

    Draft 2 - Second link

--- app.rb
+++ app.rb
@@ -2,11 +2,8 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-  
+  erb(:square_view)
+
 end
 
 get("/square/new") do
@@ -18,4 +15,15 @@ get("/square/results") do
   @the_num = params.fetch("user_input").to_f
   @the_result = @the_num ** 2
   erb(:square_result)
+end
+
+get("/square_root/new") do
+  erb(:sqroot_new)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("user_input").to_f
+  @the_result = Math.sqrt(@the_num)
+  
+  erb(:sqroot_result)
 end
