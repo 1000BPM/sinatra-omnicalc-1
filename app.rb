255d9620d56cb5a04f6877e0a680e8a8b9484fdf
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-01 22:53:54 +0000

    Draft 3 - payment link

--- app.rb
+++ app.rb
@@ -27,3 +27,30 @@ get("/square_root/results") do
   
   erb(:sqroot_result)
 end
+
+get("/payment/new") do
+  
+  erb(:payment_new)
+end
+
+get("/payment/results") do
+  #convert APR for use in formula (decimal point and monthly)
+  
+  @the_apr = params.fetch("apr_user_input").to_f
+  @monthly_apr = @the_apr / 100
+  @monthly_apr = @monthly_apr / 12
+
+  @the_yr = params.fetch("yr_user_input").to_i
+  @the_months = @the_yr * 12
+
+  @the_princ = params.fetch("pr_user_input").to_f
+  
+  @numerator = @monthly_apr * @the_princ
+  @denominator = 1 + @monthly_apr
+  @denominator = @denominator**(-1* @the_months)
+  @denominator = 1 - @denominator
+  @the_result = @numerator / @denominator
+  @the_result = @the_result.to_fs(:currency)
+  
+  erb(:payment_result)
+end
