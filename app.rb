ddb1b38924fe857f41f8248c83773567c4e7c728
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-01 23:01:00 +0000

    Fixes to formatting

--- app.rb
+++ app.rb
@@ -40,10 +40,14 @@ get("/payment/results") do
   @monthly_apr = @the_apr / 100
   @monthly_apr = @monthly_apr / 12
 
+  #need to make apr display as percent at the end; do NOT use for payment calculation
+  @the_apr = @the_apr.to_fs(:percentage)
+
   @the_yr = params.fetch("yr_user_input").to_i
   @the_months = @the_yr * 12
 
   @the_princ = params.fetch("pr_user_input").to_f
+  @the_princ_formatted = @the_princ.to_fs(:currency)
   
   @numerator = @monthly_apr * @the_princ
   @denominator = 1 + @monthly_apr
