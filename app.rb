85764f55c182befca83d811d012526ae577a9d86
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-01 21:29:12 +0000

    Draft 1 - one link done

--- app.rb
+++ app.rb
@@ -6,4 +6,16 @@ get("/") do
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+  
+end
+
+get("/square/new") do
+  erb(:square_view)
+
+end
+
+get("/square/results") do
+  @the_num = params.fetch("user_input").to_f
+  @the_result = @the_num ** 2
+  erb(:square_result)
 end
